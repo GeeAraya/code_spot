959cad82438cad2ee142641a583cd17c1fc44230
Gee Araya <GeeAraya@users.noreply.github.com>
2020-10-01 11:02:16 -0400

    Update get_json_gee_aoi.py

--- get_json_gee_aoi.py
+++ get_json_gee_aoi.py
@@ -1,5 +1,7 @@
 #python code to read a geojson file and convert it to an ee geometry obeject for AOI filters
+#this code can be added to a python script as a utility
 workingpath = 'G:\My Drive\YourJSONFile.json'
+
 json_geo_df = geopandas.read_file(workingpath)
 json_geo_js = json_geo_df.to_json()
 json_geo_js_dict = json.loads(json_geo_js)
